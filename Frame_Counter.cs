c181fa87b924f051c4e214835ef32c628a4631c3
Ryan Bates <43510534+BadBates@users.noreply.github.com>
2019-06-11 23:56:00 -0600

    this was just to make a frame counter
    this will be used and updated to make sure all projects are running smoothly

---
+++ Frame_Counter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Frame_Counter : MonoBehaviour
+{
+    public Rect fpsRect;
+    GUIStyle style;
+
+    private void Start()
+    {
+        fpsRect = new Rect(5, 5, 10, 10);
+        style = new GUIStyle();
+        style.fontSize = 10;
+    }
+
+    public void OnGUI()
+    {
+        float fps = 1 / Time.deltaTime;
+        GUI.Label(fpsRect, "FPS:" + string.Format("{0:0}", fps), style);
+    }
+}
